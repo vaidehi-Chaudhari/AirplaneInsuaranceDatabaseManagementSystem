cfe20d341cd2fdee18c9fbbd4b3896116e905d33
vaidehi-Chaudhari <68850091+vaidehi-Chaudhari@users.noreply.github.com>
2021-12-16 14:56:19 -0500

    Update project.py

--- project.py
+++ project.py
@@ -8,6 +8,28 @@ import logging
 import re
 import pandas as pd
 import hashlib
+import streamlit as st
+import base64
+
+main_bg = "img.jpg"
+main_bg_ext = "jpg"
+
+side_bg = "img.jpg"
+side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 from streamlit.type_util import OptionSequence
 #import psycopg2
